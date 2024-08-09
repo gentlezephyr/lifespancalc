e03cbb8f891ee7b098b01ebf5e0eac830dcf9e80
Elara <171523581+gentlezephyr@users.noreply.github.com>
2024-08-09 18:35:13 -0300

    Estrutura criada - Lógica do JavaScript criado

---
+++ assets/main.js
@@ -0,0 +1,37 @@
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
+
+function calculateDaysBetweenYears(startYear, endYear) {
+    let totalDays = 0;
+
+    for (let year = startYear; year < endYear; year++) {
+        totalDays += isLeapYear(year) ? 366 : 365;
+    }
+
+    return totalDays;
+}
+
+function calcYear() {
+    let getYear = (parseInt(document.querySelector('#getYear').value));
+    let endYear = 2024;
+    let startYear = getYear;
+    let totalDays = calculateDaysBetweenYears(startYear, endYear);
+
+    let totalYears = endYear - startYear;
+    let totalMonths = totalYears*12;
+
+    return {totalDays, totalMonths, totalYears};
+}
+
+function submitYear() {
+    let years = document.querySelector('#years');
+    let months = document.querySelector('#months');
+    let days = document.querySelector('#days');
+
+    let {totalDays, totalMonths, totalYears} = calcYear();
+
+    years.innerHTML = totalYears;
+    months.innerHTML = totalMonths;
+    days.innerHTML = totalDays;
+}
